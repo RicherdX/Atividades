1af02276ae6c6f550e9e24059ba16f9f9fb2ae4c
RicherdX <167588486+RicherdX@users.noreply.github.com>
2026-01-08 19:53:32 +0000

    Atividades ReStart

---
+++ AP01.py
@@ -0,0 +1,31 @@
+# Programa de saudação
+print("Hello World!")
+
+# Calculadora de soma
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+print(f"A soma de {numero1} e {numero2} é igual a {soma}")
+
+# Calculadora de volume
+comp = 12
+largura = 14
+altura = 20
+volume = 12 * 14 * 20
+print(f"O volume de uma caixa retangular de {comp}cm de comprimento, {largura}cm de largura e {altura}cm de altura é igual a {volume}cm³")
+
+# Calculadora de preço total
+nome_do_produto = str("Cadeira Infantil")
+unidades = int(3)
+preco_unitario = float(12.40)
+preco_total =  unidades * preco_unitario
+print (f"O valor total do produto {nome_do_produto}, multiplicando suas {unidades} unidades pelo seu preço unitário de R${preco_unitario:.2f}, é de R${preco_total:.2f}")
+
+# Calculadora de número inteiro
+A = int(input("Digite um número inteiro: "))
+B = int(input("Digite um número inteiro: "))
+C = int(input("Digite um número inteiro: "))
+D = int(input("Digite um número inteiro: "))
+DIFERENCA = (A * B - C * D)
+print(f"Somando a diferença de {A} * {B} e {C} * {D}")
+print(f" DIFERENCA = {DIFERENCA} ")
